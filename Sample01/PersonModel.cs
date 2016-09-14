44b1cd44737bd51500c7e382e325aaade77d974e
Walter Tudorin <walter@tudorin.net>
2016-09-14 10:27:21 +1000

    Rename Name to Firstname

--- Sample01/PersonModel.cs
+++ Sample01/PersonModel.cs
@@ -9,7 +9,7 @@ namespace Sample01
     public class Person
     {
         public int ID { get; set; }
-        public String Name { get; set; }
+        public String FirstName { get; set; }
         public String SurnName { get; set; }
 
     }
