6bb7f6c57b7a2557972422f8b473914c9908e61b
Walter Tudorin <walter@tudorin.net>
2016-09-14 11:53:48 +1000

    Add Mother

--- Sample01/PersonModel.cs
+++ Sample01/PersonModel.cs
@@ -15,6 +15,7 @@ namespace Sample01
         public Person Wife { get; set; }
         public List<Person> Children { get; set; }
         public Person Father { get; set; }
+        public Person Mother { get; set; }
 
     }
 }
